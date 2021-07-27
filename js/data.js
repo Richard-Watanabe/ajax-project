99bc8ad3cbd641a845b489dc29fb56c139fb0910
Richard-Watanabe <shoryuwatanabe124@gmail.com>
2021-07-26 17:38:18 -0700

    Made sure the movies are listed in alphabetical order for better user experience

--- js/data.js
+++ js/data.js
@@ -6,10 +6,15 @@ function getGhibliMovies() {
   var xhr = new XMLHttpRequest();
   xhr.open('GET', 'https://ghibliapi.herokuapp.com/films');
   xhr.responseType = 'json';
+  var titleArray = [];
   xhr.addEventListener('load', function () {
     for (var i = 0; i < xhr.response.length; i++) {
+      titleArray.push(xhr.response[i].title);
+      titleArray.sort();
+    }
+    for (var j = 0; j < titleArray.length; j++) {
       var $option = document.createElement('option');
-      $option.textContent = xhr.response[i].title;
+      $option.textContent = titleArray[j];
       $parent.appendChild($option);
     }
   });
