2e027f95dd855c297aa1f5f09bd6cfea0fc85793
Richard-Watanabe <shoryuwatanabe124@gmail.com>
2021-07-26 17:14:52 -0700

    Appended each movie to the select tag to have the titles show on the dropdown

--- js/data.js
+++ js/data.js
@@ -1,1 +1,19 @@
 /* exported data */
+
+var $parent = document.querySelector('select');
+
+function getGhibliMovies() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://ghibliapi.herokuapp.com/films');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    for (var i = 0; i < xhr.response.length; i++) {
+      var $option = document.createElement('option');
+      $option.textContent = xhr.response[i].title;
+      $parent.appendChild($option);
+    }
+  });
+  xhr.send();
+}
+
+getGhibliMovies();
