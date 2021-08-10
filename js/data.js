667a108e99dbca6353ae117959801c2caa9f8a9f
Richard Watanabe <shoryuwatanabe124@gmail.com>
2021-08-10 20:06:40 +0000

    Changed all vars to let and const

--- js/data.js
+++ js/data.js
@@ -1,21 +1,21 @@
 /* exported ghibliData */
 
-var ghibliData = {
+let ghibliData = {
   view: 'home-screen',
   reviews: [],
   editing: null,
   nextReviewId: 1
 };
 
-var oldReviewJSON = localStorage.getItem('ghibli-local-storage');
+const oldReviewJSON = localStorage.getItem('ghibli-local-storage');
 
 if (oldReviewJSON !== null) {
-  var oldData = JSON.parse(oldReviewJSON);
+  const oldData = JSON.parse(oldReviewJSON);
   ghibliData = oldData;
 }
 
 function saveData(event) {
-  var newReview = JSON.stringify(ghibliData);
+  const newReview = JSON.stringify(ghibliData);
   localStorage.setItem('ghibli-local-storage', newReview);
 }
 
--- js/main.js
+++ js/main.js
@@ -1,37 +1,37 @@
 /* global ghibliData */
 /* exported ghibliData */
 
-var $parent = document.querySelector('select');
-var $search = document.querySelector('.search');
-var $reviewParent = document.querySelector('.parent');
-var $view = document.querySelectorAll('.view');
-var $reviewForm = document.querySelector('.review-text');
-var $header = document.querySelector('header');
-var $myReviewNav = document.querySelector('.my-review-nav');
-var $myReview = document.querySelector('.my-review');
-var $logoNav = document.querySelector('.nav-logo');
-var $newButton = document.querySelector('.add-new-button');
-var $noReview = document.querySelector('.no-review');
-var $ulParent = document.querySelector('ul');
-var $reviewNotes = document.querySelector('#review');
-var $divModal = document.querySelector('.modal');
-var $cancel = document.querySelector('.cancel');
-var $confirm = document.querySelector('.confirm');
-var $loading = document.querySelector('.spin');
-var $introModal = document.querySelector('.intro-modal');
-var $ok = document.querySelector('.ok');
-var $countParent = document.querySelector('.count-parent');
-var $countText = document.querySelector('.count-text');
-var $number = document.querySelector('.number');
-var $allNumber = document.querySelector('.all-number');
+const $parent = document.querySelector('select');
+const $search = document.querySelector('.search');
+const $reviewParent = document.querySelector('.parent');
+const $view = document.querySelectorAll('.view');
+const $reviewForm = document.querySelector('.review-text');
+const $header = document.querySelector('header');
+const $myReviewNav = document.querySelector('.my-review-nav');
+const $myReview = document.querySelector('.my-review');
+const $logoNav = document.querySelector('.nav-logo');
+const $newButton = document.querySelector('.add-new-button');
+const $noReview = document.querySelector('.no-review');
+const $ulParent = document.querySelector('ul');
+const $reviewNotes = document.querySelector('#review');
+const $divModal = document.querySelector('.modal');
+const $cancel = document.querySelector('.cancel');
+const $confirm = document.querySelector('.confirm');
+const $loading = document.querySelector('.spin');
+const $introModal = document.querySelector('.intro-modal');
+const $ok = document.querySelector('.ok');
+const $countParent = document.querySelector('.count-parent');
+const $countText = document.querySelector('.count-text');
+const $number = document.querySelector('.number');
+const $allNumber = document.querySelector('.all-number');
 
-var $posterLink = document.createElement('img');
-var $title = document.createElement('div');
-var $description = document.createElement('div');
-var $rowDiv = document.createElement('div');
-var $reviewTitle = document.createElement('div');
-var $descriptionTitle = document.createElement('div');
-var $columnDiv = document.createElement('div');
+const $posterLink = document.createElement('img');
+const $title = document.createElement('div');
+const $description = document.createElement('div');
+const $rowDiv = document.createElement('div');
+const $reviewTitle = document.createElement('div');
+const $descriptionTitle = document.createElement('div');
+const $columnDiv = document.createElement('div');
 
 $search.addEventListener('submit', populateForm);
 $reviewForm.addEventListener('submit', saveReview);
@@ -51,20 +51,20 @@ window.addEventListener('load', openIntro);
 $ok.addEventListener('click', closeIntroModal);
 
 function populateSearchBar(parent) {
-  var xhr = new XMLHttpRequest();
+  const xhr = new XMLHttpRequest();
   xhr.open('GET', 'https://ghibliapi.herokuapp.com/films');
   $loading.className = 'spin';
   xhr.responseType = 'json';
-  var titleArray = [];
+  const titleArray = [];
   xhr.addEventListener('load', function () {
-    for (var i = 0; i < xhr.response.length; i++) {
+    for (let i = 0; i < xhr.response.length; i++) {
       $loading.className = 'spin hidden';
       titleArray.push(xhr.response[i].title);
       titleArray.sort();
     }
-    for (var j = 0; j < titleArray.length; j++) {
+    for (let j = 0; j < titleArray.length; j++) {
       $loading.className = 'spin hidden';
-      var $option = document.createElement('option');
+      const $option = document.createElement('option');
       $option.textContent = titleArray[j];
       $parent.appendChild($option);
     }
@@ -79,8 +79,8 @@ populateSearchBar($countParent);
 
 function populateForm(event) {
   event.preventDefault();
-  var title = $search.elements.title.value;
-  var addForm = createForm(title);
+  const title = $search.elements.title.value;
+  const addForm = createForm(title);
   $reviewParent.prepend(addForm);
   addPoster($search.elements.title.value, $columnDiv);
   switchView('review-form');
@@ -92,7 +92,7 @@ function populateForm(event) {
 
 function saveReview(event) {
   event.preventDefault();
-  var dataObject = {
+  const dataObject = {
     title: $title.textContent,
     image: $posterLink.getAttribute('src'),
     description: $description.textContent,
@@ -106,7 +106,7 @@ function saveReview(event) {
   if ($reviewNotes.textContent === '') {
     ghibliData.reviews.unshift(dataObject);
     $reviewParent.removeChild($rowDiv);
-    var addForm = createReview(dataObject);
+    const addForm = createReview(dataObject);
     $ulParent.prepend(addForm);
     movieMatchCount($countParent);
     $noReview.className = 'hidden';
@@ -116,11 +116,11 @@ function saveReview(event) {
     ghibliData.editing.image = $posterLink.getAttribute('src');
     ghibliData.editing.description = $description.textContent;
     ghibliData.editing.review = $reviewForm.elements.review.value;
-    for (var i = 0; i < ghibliData.reviews.length; i++) {
+    for (let i = 0; i < ghibliData.reviews.length; i++) {
       if (ghibliData.reviews[i].reviewId === ghibliData.editing.reviewId) {
         ghibliData.reviews[i] = ghibliData.editing;
-        var addEdit = createReview(ghibliData.editing);
-        var $list = document.querySelectorAll('[data-review-id]');
+        const addEdit = createReview(ghibliData.editing);
+        const $list = document.querySelectorAll('[data-review-id]');
         $list[i].replaceWith(addEdit);
         switchView('review-gallery');
         $reviewForm.reset();
@@ -130,14 +130,14 @@ function saveReview(event) {
 }
 
 function addPoster(movieName, parent) {
-  var xhr = new XMLHttpRequest();
+  const xhr = new XMLHttpRequest();
   xhr.open('GET', 'https://api.themoviedb.org/3/search/movie?api_key=e2317d1150ebe694b173d9560f5e95b8&query=' + movieName);
   $loading.className = 'spin';
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
-    var posterPath;
+    let posterPath;
     $loading.className = 'spin hidden';
-    for (var i = 0; i < xhr.response.results.length; i++) {
+    for (let i = 0; i < xhr.response.results.length; i++) {
       if (xhr.response.results[i].title === 'Spirited Away' || xhr.response.results[i].title === 'Grave of the Fireflies' || xhr.response.results[i].title === 'Pom Poko') {
         posterPath = xhr.response.results[1].poster_path;
       } else {
@@ -155,15 +155,15 @@ function addPoster(movieName, parent) {
 }
 
 function addDescription(parent, insertBefore) {
-  var xhr = new XMLHttpRequest();
+  const xhr = new XMLHttpRequest();
   xhr.open('GET', 'https://ghibliapi.herokuapp.com/films');
   $loading.className = 'spin';
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
     $loading.className = 'spin hidden';
-    for (var i = 0; i < xhr.response.length; i++) {
+    for (let i = 0; i < xhr.response.length; i++) {
       if ($title.textContent === xhr.response[i].title) {
-        var descriptionUnique = xhr.response[i].description;
+        const descriptionUnique = xhr.response[i].description;
         $description.setAttribute('class', 'description-text text');
         $description.textContent = descriptionUnique;
         parent.insertBefore($description, insertBefore);
@@ -178,7 +178,7 @@ function addDescription(parent, insertBefore) {
 
 function switchView(name) {
   ghibliData.view = name;
-  for (var i = 0; i < $view.length; i++) {
+  for (let i = 0; i < $view.length; i++) {
     if (name !== 'home-screen') {
       $header.className = 'background-blue';
     }
@@ -201,7 +201,7 @@ function createForm(title) {
   $descriptionTitle.setAttribute('class', 'sub-blue review-bar white-text');
   $descriptionTitle.textContent = 'Description:';
 
-  var $columnDiv2 = document.createElement('div');
+  const $columnDiv2 = document.createElement('div');
   $columnDiv2.setAttribute('class', 'column-half');
 
   $reviewTitle.setAttribute('class', 'sub-blue review-bar white-text');
@@ -222,48 +222,48 @@ function createForm(title) {
 }
 
 function createReview(review) {
-  var $newList = document.createElement('li');
+  const $newList = document.createElement('li');
   $newList.setAttribute('data-review-id', review.reviewId);
 
-  var $newRowDiv = document.createElement('div');
+  const $newRowDiv = document.createElement('div');
   $newRowDiv.setAttribute('class', 'row');
 
-  var $newColumnDiv = document.createElement('div');
+  const $newColumnDiv = document.createElement('div');
   $newColumnDiv.setAttribute('class', 'column-half');
 
-  var $newTitle = document.createElement('div');
+  const $newTitle = document.createElement('div');
   $newTitle.setAttribute('class', 'title-blue review-bar white-text italic row space-between');
   $newTitle.textContent = review.title;
 
-  var $iconDiv = document.createElement('div');
+  const $iconDiv = document.createElement('div');
   $iconDiv.setAttribute('class', 'align-right');
 
-  var $editIcon = document.createElement('i');
+  const $editIcon = document.createElement('i');
   $editIcon.setAttribute('class', 'far fa-edit');
 
-  var $deleteIcon = document.createElement('i');
+  const $deleteIcon = document.createElement('i');
   $deleteIcon.setAttribute('class', 'far fa-window-close');
 
-  var $newImage = document.createElement('img');
+  const $newImage = document.createElement('img');
   $newImage.setAttribute('src', review.image);
   $newImage.setAttribute('class', 'ghibli-image');
 
-  var $newDescriptionTitle = document.createElement('div');
+  const $newDescriptionTitle = document.createElement('div');
   $newDescriptionTitle.setAttribute('class', 'sub-blue review-bar white-text');
   $newDescriptionTitle.textContent = 'Description:';
 
-  var $newDescription = document.createElement('div');
+  const $newDescription = document.createElement('div');
   $newDescription.textContent = review.description;
   $newDescription.setAttribute('class', 'description-text text');
 
-  var $newColumnDiv2 = document.createElement('div');
+  const $newColumnDiv2 = document.createElement('div');
   $newColumnDiv2.setAttribute('class', 'column-half');
 
-  var $newReviewTitle = document.createElement('div');
+  const $newReviewTitle = document.createElement('div');
   $newReviewTitle.setAttribute('class', 'sub-blue review-bar white-text');
   $newReviewTitle.textContent = 'Review:';
 
-  var $newReviewText = document.createElement('div');
+  const $newReviewText = document.createElement('div');
   $newReviewText.setAttribute('class', 'text review-notes');
   $newReviewText.textContent = review.review;
 
@@ -289,8 +289,8 @@ function createReview(review) {
 }
 
 function addReview(event) {
-  for (var i = 0; i < ghibliData.reviews.length; i++) {
-    var newReview = createReview(ghibliData.reviews[i]);
+  for (let i = 0; i < ghibliData.reviews.length; i++) {
+    const newReview = createReview(ghibliData.reviews[i]);
     $ulParent.appendChild(newReview);
   }
 }
@@ -324,14 +324,14 @@ function refreshGoHome(event) {
 
 function showEditForm(event) {
   if (event.target.matches('.fa-edit')) {
-    var stringId = event.target.closest('li').getAttribute('data-review-id');
-    for (var i = 0; i < ghibliData.reviews.length; i++) {
+    const stringId = event.target.closest('li').getAttribute('data-review-id');
+    for (let i = 0; i < ghibliData.reviews.length; i++) {
       if (ghibliData.reviews[i].reviewId === parseInt(stringId)) {
         ghibliData.editing = ghibliData.reviews[i];
       }
     }
-    var title = $search.elements.title.value;
-    var addForm = createForm(title);
+    const title = $search.elements.title.value;
+    const addForm = createForm(title);
     $reviewParent.prepend(addForm);
     if ($rowDiv.childElementCount > 2) {
       $rowDiv.firstElementChild.remove();
@@ -350,8 +350,8 @@ function showEditForm(event) {
 function openModal(event) {
   if (event.target.matches('.fa-window-close')) {
     $divModal.className = 'modal';
-    var stringId = event.target.closest('li').getAttribute('data-review-id');
-    for (var i = 0; i < ghibliData.reviews.length; i++) {
+    const stringId = event.target.closest('li').getAttribute('data-review-id');
+    for (let i = 0; i < ghibliData.reviews.length; i++) {
       if (ghibliData.reviews[i].reviewId === parseInt(stringId)) {
         ghibliData.editing = ghibliData.reviews[i];
       }
@@ -364,11 +364,11 @@ function closeModal(event) {
 }
 
 function deleteReview(event) {
-  for (var i = 0; i < ghibliData.reviews.length; i++) {
+  for (let i = 0; i < ghibliData.reviews.length; i++) {
     if (ghibliData.reviews[i].reviewId === ghibliData.editing.reviewId) {
       ghibliData.reviews[i] = ghibliData.editing;
       ghibliData.reviews.splice(i, 1);
-      var $list = document.querySelectorAll('[data-review-id]');
+      const $list = document.querySelectorAll('[data-review-id]');
       $list[i].remove($list[i]);
       switchView('review-gallery');
       $divModal.className = 'modal hidden';
@@ -393,22 +393,22 @@ function closeIntroModal(event) {
 }
 
 function movieMatchCount(parent, insertBefore) {
-  var xhr = new XMLHttpRequest();
+  const xhr = new XMLHttpRequest();
   xhr.open('GET', 'https://ghibliapi.herokuapp.com/films');
   $loading.className = 'spin';
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
     $loading.className = 'spin hidden';
-    var titleArray = [];
-    var titleArray2 = [];
-    for (var i = 0; i < xhr.response.length; i++) {
+    const titleArray = [];
+    const titleArray2 = [];
+    for (let i = 0; i < xhr.response.length; i++) {
       titleArray.push(xhr.response[i].title);
     }
-    for (var j = 0; j < ghibliData.reviews.length; j++) {
+    for (let j = 0; j < ghibliData.reviews.length; j++) {
       titleArray2.push(ghibliData.reviews[j].title);
     }
-    var allNumber = titleArray.length;
-    var number = countCheck(titleArray, titleArray2);
+    const allNumber = titleArray.length;
+    const number = countCheck(titleArray, titleArray2);
     $number.textContent = number;
     $allNumber.textContent = allNumber;
   });
@@ -419,9 +419,9 @@ function movieMatchCount(parent, insertBefore) {
 }
 
 function countCheck(first, second) {
-  var newArray = [];
-  for (var i = 0; i < first.length; i++) {
-    for (var j = 0; j < second.length; j++) {
+  const newArray = [];
+  for (let i = 0; i < first.length; i++) {
+    for (let j = 0; j < second.length; j++) {
       if (first[i] === second[j] && !newArray.includes(first[i])) {
         newArray.push(first[i]);
       }
